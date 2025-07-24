5b0f6b338acf215c40fc1eeffe85d9e71e2a0eb3
tinsez <milo@tinsez.art>
2025-07-24 00:10:35 -0600

    flexbox updates

--- script.js
+++ script.js
@@ -200,3 +200,11 @@ function newColour() {
 
 // ]]>
 
+function clickEffect(e){
+var d=document.createElement("div");
+d.className="clickEffect";
+d.style.top=e.clientY+"px";d.style.left=e.clientX+"px";
+document.body.appendChild(d);
+d.addEventListener('animationend',function(){d.parentElement.removeChild(d);}.bind(this));
+}
+document.addEventListener('click',clickEffect);
